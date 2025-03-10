66076ab1ff77e27d0a11971ffb6e67c8a6857db2
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-03-09 22:07:31 -0300

    adicionando cores personalizadas ao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
 	],
 	presets: [require('nativewind/preset')],
 	theme: {
-	  extend: {},
+	  extend: {
+		colors: {
+			'l-yellow': '#FFF8B0', // Light Yellow: Primary background or subtle details
+			'm-blue': '#4F89B4', // Medium Blue: Highlights, titles, and buttons
+			'l-blue': '#9AAED8', // Light Blue: Secondary highlights and alternative buttons
+			'l-gray': '#E5E7EB', // Light Gray: Backgrounds for lists and separators
+			'black': '#1A1A1A', // Black: Main text and some element backgrounds
+			'red-alert': '#B24E3F'  // Brick Red: Icons, alerts, and action buttons
+		  }
+	  },
 	},
 	plugins: [],
   };
